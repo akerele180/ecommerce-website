57046783d7b90d64cd44a7460c9b4317d5f4df78
Tosin Akerele <akerele.james1699@gmail.com>
2021-06-29 14:16:56 +0100

    Addition of new files like the shop page, collection item and collection preview components.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
-import Homepage from './pages/hompage.component'
+import { Route } from 'react-router-dom';
+
+import Homepage from './pages/hompage.component';
+import ShopPage from './pages/shop/shop.component.jsx'
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <Homepage /> 
+      <Route exact path='/' component={ Homepage }/>
+      <Route path='/hatspage' component={ HatsPage }/>
+      <Route path='/shop' component={ ShopPage }/>
     </div>
   )
 }
--- src/components/menu-item/menu-items.component.jsx
+++ src/components/menu-item/menu-items.component.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
+import React from 'react';
 import './menu-items.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => (
-  <div className= { `${size} menu-item`}>
-    <div 
-      className='background-image' 
-      style = {{ backgroundImage: `url(${imageUrl})` 
-      }} 
-    />
-    <div className="content">
-      <h1 className = "title">{ title.toUpperCase() }</h1>
-      <span className = "subtitle">SHOP NOW</span>
+  // Note that the destructured items (i.e. {title, imageUrl, size}) are all props.
+  
+  <>
+    <div className={`${size} menu-item`}>
+      <div
+        className='background-image'
+        style={{
+          // backgroundImage: {styles}
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
+      <div className='content'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
     </div>
-  </div> 
+  </>
 );
 
 export default MenuItem;
