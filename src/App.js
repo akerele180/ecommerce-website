3f29db0f0725b91cb565f8688110c229ce783dea
Tosin Akerele <akerele.james1699@gmail.com>
2021-06-29 16:14:34 +0100

    Addition of the header component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import Homepage from './pages/hompage.component';
-import ShopPage from './pages/shop/shop.component.jsx'
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 const HatsPage = () => (
   <div>
@@ -13,11 +14,14 @@ const HatsPage = () => (
 
 function App() {
   return (
-    <div>
-      <Route exact path='/' component={ Homepage }/>
-      <Route path='/hatspage' component={ HatsPage }/>
-      <Route path='/shop' component={ ShopPage }/>
-    </div>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={ Homepage }/>
+        <Route path='/hatspage' component={ HatsPage }/>
+        <Route path='/shop' component={ ShopPage }/>
+      </Switch>
+    </>
   )
 }
 
